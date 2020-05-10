01a743a5e8d075463123c6beed723dc058010695
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 22:45:46 +0300

    add MulCommand.execute

--- app/src/main/java/ru/geekbrains/hellocalculator/calc/MulCommand.java
+++ app/src/main/java/ru/geekbrains/hellocalculator/calc/MulCommand.java
@@ -2,11 +2,16 @@ package ru.geekbrains.hellocalculator.calc;
 
 public class MulCommand implements ICommand {
 
+    private final double arg1;
+    private final double arg2;
+
     public MulCommand(double arg1, double arg2) {
+        this.arg1 = arg1;
+        this.arg2 = arg2;
     }
 
     @Override
     public double execute() {
-        return 0;
+        return arg1 * arg2;
     }
 }
