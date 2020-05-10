4b523fcee96e3333f62379cb82ae7c8d02855fc2
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 22:40:02 +0300

    add test for execute in SubClass

--- app/src/test/java/ru/geekbrains/hellocalculator/SubCommandUnitTest.java
+++ app/src/test/java/ru/geekbrains/hellocalculator/SubCommandUnitTest.java
@@ -4,10 +4,18 @@ import org.junit.Test;
 
 import ru.geekbrains.hellocalculator.calc.SubCommand;
 
+import static org.junit.Assert.assertEquals;
+
 public class SubCommandUnitTest {
 
     @Test(expected = Test.None.class)
     public void constructor_isCorrect(){
         SubCommand underTest = new SubCommand(.0, .0);
+    }
+
+    @Test
+    public void execute_isCorrect(){
+        SubCommand underTest = new SubCommand(5., 2.);
+        assertEquals(3., underTest.execute(), 0);
     }
 }
