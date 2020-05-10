c8697d7f884a1315a98790477765a7ed1cf9dcd1
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 22:43:39 +0300

    add multiplication command class

---
+++ app/src/main/java/ru/geekbrains/hellocalculator/calc/MulCommand.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.hellocalculator.calc;
+
+public class MulCommand implements ICommand {
+
+    public MulCommand(double arg1, double arg2) {
+    }
+
+    @Override
+    public double execute() {
+        return 0;
+    }
+}
