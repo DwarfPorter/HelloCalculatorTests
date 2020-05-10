e371b74829b63307bf1f57965521f58f3a7cead8
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 21:59:51 +0300

    add test for executeCommand in calculator with use mock of ICommand

--- app/src/test/java/ru/geekbrains/hellocalculator/CalculatorUnitTest.java
+++ app/src/test/java/ru/geekbrains/hellocalculator/CalculatorUnitTest.java
@@ -3,8 +3,11 @@ package ru.geekbrains.hellocalculator;
 import org.junit.Test;
 
 import ru.geekbrains.hellocalculator.calc.Calculator;
+import ru.geekbrains.hellocalculator.calc.ICommand;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -16,5 +19,13 @@ public class CalculatorUnitTest {
     @Test(expected = Test.None.class)
     public void constructor_isCorrect() {
         Calculator underTest = new Calculator();
+    }
+
+    @Test
+    public void executeCommand_isCorrect(){
+        Calculator underTest = new Calculator();
+        ICommand command = mock(ICommand.class);
+        underTest.executeCommand(command);
+        verify(command).execute();
     }
 }
