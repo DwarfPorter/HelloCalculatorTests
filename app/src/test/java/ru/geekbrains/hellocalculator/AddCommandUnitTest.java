33e29797364d60e4f80f7d8a1c95634a4eaea6f9
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 22:31:51 +0300

    Add test for AddCommand.execute

--- app/src/test/java/ru/geekbrains/hellocalculator/AddCommandUnitTest.java
+++ app/src/test/java/ru/geekbrains/hellocalculator/AddCommandUnitTest.java
@@ -2,13 +2,21 @@ package ru.geekbrains.hellocalculator;
 
 import org.junit.Test;
 
-import ru.geekbrains.hellocalculator.calc.ICommand;
+import ru.geekbrains.hellocalculator.calc.AddCommand;
+
+import static org.junit.Assert.*;
 
 public class AddCommandUnitTest {
 
     @Test(expected = Test.None.class)
     public void constructor_isCorrect(){
         AddCommand underTest = new AddCommand(.0, .0);
+    }
+
+    @Test
+    public void execute_isCorrect(){
+        AddCommand underTest = new AddCommand(2., 2.);
+        assertEquals(4., underTest.execute(), 0);
     }
 
 }
