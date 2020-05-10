f22a0ede0441f75ff2b53123784e877f5dd5c37d
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 22:49:17 +0300

    add test for execute in DivCommand

--- app/src/test/java/ru/geekbrains/hellocalculator/DivCommandUnitTest.java
+++ app/src/test/java/ru/geekbrains/hellocalculator/DivCommandUnitTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 import ru.geekbrains.hellocalculator.calc.DivCommand;
 import ru.geekbrains.hellocalculator.calc.MulCommand;
 
+import static org.junit.Assert.assertEquals;
+
 public class DivCommandUnitTest {
 
     @Test(expected = Test.None.class)
     public void constructor_isCorrect(){
         DivCommand underTest = new DivCommand(.1, .1);
+    }
+
+    @Test
+    public void execute_isCorrect(){
+        DivCommand underTest = new DivCommand(15., 3.);
+        assertEquals(5., underTest.execute(), 0);
     }
 }
