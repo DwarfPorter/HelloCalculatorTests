4306b6bcc3988338e8a7a19ba96747de2d3accbb
DwarfPorter <mrvlmor@gmail.com>
2020-05-10 22:40:50 +0300

    add execute in SubClass

--- app/src/main/java/ru/geekbrains/hellocalculator/calc/SubCommand.java
+++ app/src/main/java/ru/geekbrains/hellocalculator/calc/SubCommand.java
@@ -2,12 +2,17 @@ package ru.geekbrains.hellocalculator.calc;
 
 public class SubCommand implements ICommand {
 
+    private final double arg1;
+    private final double arg2;
+
     public SubCommand(double arg1, double arg2){
 
+        this.arg1 = arg1;
+        this.arg2 = arg2;
     }
 
     @Override
     public double execute() {
-        return 0;
+        return arg1 - arg2;
     }
 }
